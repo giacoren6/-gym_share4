3b988e91367e0a3ec54e1a4d5830d01743de3b41
giacoren6 <142323106+giacoren6@users.noreply.github.com>
2024-11-18 12:40:59 +0000

    adding the models to the apps

--- myproject/comments/models.py
+++ myproject/comments/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from posts.models import Post
+from profiles.models import Profile
 
-# Create your models here.
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    content = models.TextField()
+
+    def __str__(self):
+        return self.content[:20]
--- myproject/posts/models.py
+++ myproject/posts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from profiles.models import Profile
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    author = models.ForeignKey(Profile, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
