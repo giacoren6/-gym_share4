920f42ac134ecbd18282f946733a1063541ddcde
giacoren6 <142323106+giacoren6@users.noreply.github.com>
2024-11-18 12:45:15 +0000

    adding views and urls in myproject

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -14,10 +14,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from profiles.views import ProfileViewSet
+from posts.views import PostViewSet
+from comments.views import CommentViewSet
+
+router = DefaultRouter()
+router.register(r'profiles', ProfileViewSet)
+router.register(r'posts', PostViewSet)
+router.register(r'comments', CommentViewSet)
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
 ]
